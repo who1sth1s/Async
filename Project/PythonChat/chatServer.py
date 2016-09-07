4767757f659472431b8a740f9c366b0829351c2f
who1sth1s <ksjlike69@gmail.com>
2016-09-07 23:53:12 +0900

    Add setting

--- Project/PythonChat/chatServer.py
+++ Project/PythonChat/chatServer.py
@@ -8,3 +8,6 @@ PORT = 33137
 
 def chatServer():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(10)
