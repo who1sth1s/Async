f57c35e1127680c056d204749e004a14565c3be8
who1sth1s <ksjlike69@gmail.com>
2016-09-08 23:54:10 +0900

    PythonChat/chatServer.py : add async and code

--- Project/PythonChat/chatServer.py
+++ Project/PythonChat/chatServer.py
@@ -1,13 +1,24 @@
 import asyncio
 import socket
+import sys
 
 HOST = ''
 SOCKET_LIST = []
 RECV_BUFFER = 4096
 PORT = 33137
 
-def chatServer():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((HOST, PORT))
-    server_socket.listen(10)
+async def chatServer():
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serverSocket.bind((HOST, PORT))
+    serverSocket.listen(10)
+    SOCKET_LIST.append(serverSocket)
+    print('[Chat server]')
+    print('[Port]{PORT}').format(PORT=PORT)
+
+
+
+
+if __name__ == '__main__':
+    await chatServer()
+    sys.exit(chatServer())
