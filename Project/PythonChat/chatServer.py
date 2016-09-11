e1831feadba990d8a665dd00d5883722b114ed24
who1sth1s <ksjlike69@gmail.com>
2016-09-11 14:07:36 +0900

    PythonChat/chatServer.py: async -> @asyncio.coroutine

--- Project/PythonChat/chatServer.py
+++ Project/PythonChat/chatServer.py
@@ -1,4 +1,5 @@
 import asyncio
+import traceback
 import socket
 import sys
 
@@ -7,18 +8,25 @@ SOCKET_LIST = []
 RECV_BUFFER = 4096
 PORT = 33137
 
-async def chatServer():
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    serverSocket.bind((HOST, PORT))
-    serverSocket.listen(10)
-    SOCKET_LIST.append(serverSocket)
-    print('[Chat server]')
-    print('[Port]{PORT}').format(PORT=PORT)
+@asyncio.coroutine
+def chatServer():
+    try:
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        serverSocket.bind((HOST, PORT))
+        serverSocket.listen(10)
+        SOCKET_LIST.append(serverSocket)
+        return HOST
+    except:
+        print(traceback._format_final_exc_line())
 
-
-
+@asyncio.coroutine
+def main(argv):
+    try:
+        HOST = yield from chatServer()
+        print(HOST)
+    except:
+        print(traceback._format_final_exc_line())
 
 if __name__ == '__main__':
-    await chatServer()
-    sys.exit(chatServer())
+    sys.exit(main(sys.argv))
