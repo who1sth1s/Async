3fcb05936168f05c03cffc9503c621f4f88f391d
who1sth1s <ksjlike69@gmail.com>
2016-09-15 22:21:28 +0900

    PythonChat/chatServer.py: Add chatServerSetting function

--- Project/PythonChat/chatServer.py
+++ Project/PythonChat/chatServer.py
@@ -11,13 +11,18 @@ class ChatServer():
         pass
 
     @asyncio.coroutine
-    def chatServer(self):
+    def chatServerMain(self):
         try:
-            serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            serverSocket.bind((self.HOST, self.PORT))
-            serverSocket.listen(10)
+            serverSocket = yield from self.chatServerSetting()
             self.SOCKET_LIST.append(serverSocket)
             print('Chat server started on port ' + str(self.PORT))
         except:
             print(traceback.format_exc().strip().split('\n'))
+
+    @asyncio.coroutine
+    def chatServerSetting(self):
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        serverSocket.bind((self.HOST, self.PORT))
+        serverSocket.listen(10)
+        return serverSocket
