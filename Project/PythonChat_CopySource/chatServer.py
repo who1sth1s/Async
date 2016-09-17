5bcfa3b9a9e3bd0a18a50e9843d2b32df07c0eb9
who1sth1s <ksjlike69@gmail.com>
2016-09-17 19:13:52 +0900

    Change folder name PythonChat -> PythonChat_Copy

--- Project/PythonChat_CopySource/chatServer.py
+++ Project/PythonChat_CopySource/chatServer.py
@@ -18,9 +18,18 @@ class ChatServer():
             self.SOCKET_LIST.append(serverSocket)
             print('Chat server started on port ' + str(self.PORT))
             while 1:
-                ready_to_ready, \
+                ready_to_read, \
                 ready_to_write, \
                 in_error = select.select(self.SOCKET_LIST, [], [], 0)
+
+                for sock in ready_to_read:
+                    if sock == serverSocket:
+                        sockfd, addr = serverSocket.accept()
+                        self.SOCKET_LIST.append(sockfd)
+                        print('Client ({addr}, {addr})').format(addr=addr)
+                        yield from self.broadcast(serverSocket, sockfd,
+                                                  '[{addr}:{addr}] entered our chatting room\n'.format(addr=addr))
+
         except:
             print(traceback.format_exc().strip().split('\n'))
 
@@ -31,3 +40,14 @@ class ChatServer():
         serverSocket.bind((self.HOST, self.PORT))
         serverSocket.listen(10)
         return serverSocket
+
+    @asyncio.coroutine
+    def broadcast(self, serverSocket, sock, message):
+        for socket in self.SOCKET_LIST:
+            if socket != serverSocket and socket != sock:
+                try:
+                    socket.send(message)
+                except:
+                    socket.close()
+                    if socket in self.SOCKET_LIST:
+                        self.SOCKET_LIST.remove(socket)
