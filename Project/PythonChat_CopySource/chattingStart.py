8c7aaf1bd11f96720e3404d43d78755f4a6444bd
who1sth1s <ksjlike69@gmail.com>
2016-09-19 21:47:58 +0900

    PythonChat_CopySource/chattingStart.py: join server and create server setting

--- Project/PythonChat_CopySource/chattingStart.py
+++ Project/PythonChat_CopySource/chattingStart.py
@@ -14,7 +14,13 @@ class ChattingStart():
     @asyncio.coroutine
     def chattingStart(self):
         #self.chatServer = yield from chatServer.ChatServer()
-        self.HOST = yield from chatServer.ChatServer().chatServerMain()
+        selectType = input('(--- Input your type ---)\n1. Create Server\n2. Join Server\n:')
+        if selectType == 'Create':
+            self.HOST = yield from chatServer.ChatServer().chatServerMain()
+        elif selectType == 'Join':
+            print('Hello world!!')
+        else:
+            print('''\nIf you want to create server, input "Create"\nIf you want to join server, input "Join"''')
 
 chattingClass = ChattingStart()
 asyncio.get_event_loop().run_until_complete(chattingClass.chattingStart())
