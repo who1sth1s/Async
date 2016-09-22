4b6df70cbcb584d0043fd13a7bb1851b792ddd29
who1sth1s <ksjlike69@gmail.com>
2016-09-23 01:16:33 +0900

    PythonChat_CopySource/chatClient.py : Fix error and add traceback

--- Project/PythonChat_CopySource/chatClient.py
+++ Project/PythonChat_CopySource/chatClient.py
@@ -2,6 +2,7 @@ import asyncio
 import sys
 import socket
 import select
+import traceback
 
 class ChatClient():
     def __init__(self):
@@ -11,10 +12,11 @@ class ChatClient():
     def chatClient(self, inputHost, inputPort):
         createSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         createSocket.settimeout(2)
+        print(createSocket)
         try:
             createSocket.connect((inputHost, int(inputPort)))
         except:
-            print('Unable to connect')
+            print(traceback.format_exc())
             sys.exit()
 
         print('Connected to remote host. You can start sending messages')
@@ -26,12 +28,14 @@ class ChatClient():
             ready_to_read, ready_to_write, in_error = select.select(SOCKET_LIST, [], [])
             for sock in ready_to_read:
                 if sock == createSocket:
-                    data = sock.recv(4096)
+                    data = sock.recv(4096).decode()
                     if not data:
                         print('\nDisconnected from chat server')
+                        createSocket.close()
                         sys.exit()
                     else:
                         sys.stdout.write(data)
+                        print(data)
                         sys.stdout.write('[Me] '); sys.stdout.flush()
 
                 else:
