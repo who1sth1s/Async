19c925d98553215568afa4e1909bd232583baeaf
who1sth1s <ksjlike69@gmail.com>
2016-10-01 20:52:54 +0900

    DC_InsideBestPost_Parsing.py: Set url to dictionary(json )

--- Project/DC_InsideBestPost_Parsing/DC_InsideBestPost_Parsing.py
+++ Project/DC_InsideBestPost_Parsing/DC_InsideBestPost_Parsing.py
@@ -1,6 +1,8 @@
 import asyncio
+import sys
 
+import Project.DC_InsideBestPost_Parsing.urlList
 from urllib import request
 from bs4 import BeautifulSoup
 
-url = "http://gall.dcinside.com/board/lists/?id=football_new5&page=1&exception_mode=recommend"
+sys.path.append('../')
---
+++ Project/DC_InsideBestPost_Parsing/urlList.py
@@ -0,0 +1,4 @@
+url = {
+    '해외축구': 'http://gall.dcinside.com/board/lists/?id=football_new5',
+    '최유정': 'http://gall.dcinside.com/board/lists/?id=youjung'
+}
