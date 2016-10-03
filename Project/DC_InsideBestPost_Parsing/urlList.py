d43f68dcdd7229f9017e5638bc583e75c3043337
who1sth1s <ksjlike69@gmail.com>
2016-10-04 00:19:31 +0900

    urlList.py: Setting url list function

--- Project/DC_InsideBestPost_Parsing/urlList.py
+++ Project/DC_InsideBestPost_Parsing/urlList.py
@@ -1,4 +1,17 @@
-url = {
-    '해외축구': 'http://gall.dcinside.com/board/lists/?id=football_new5',
-    '최유정': 'http://gall.dcinside.com/board/lists/?id=youjung'
-}
+# -*-coding: utf-8 -*-
+import sys
+
+class UrlList():
+    def __init__(self):
+        pass
+
+    def urlList(self, key):
+        url = {
+            '해외축구': 'http://gall.dcinside.com/board/lists/?id=football_new5',
+            '최유정': 'http://gall.dcinside.com/board/lists/?id=youjung'
+        }
+        if not key in url:
+            print('No galary, select other galary')
+            sys.exit()
+        else:
+            return url[key]
