7d311ee826af8609c79ffaf7e5414acd5f323f25
who1sth1s <ksjlike69@gmail.com>
2016-10-14 00:48:26 +0900

    urlParsing.py: Get html source

--- Project/DC_InsideBestPost_Parsing/urlParsing.py
+++ Project/DC_InsideBestPost_Parsing/urlParsing.py
@@ -1,3 +1,5 @@
+# -*-coding: utf-8-*-
+
 import asyncio
 import traceback
 import sys
@@ -8,11 +10,17 @@ class UrlParsingClass():
         pass
 
     @asyncio.coroutine
+    def mainParser(self, galleryUrl):
+        htmlSource = yield from self.getHtml(galleryUrl)
+
+    @asyncio.coroutine
     def getHtml(self, galleryUrl):
         pageNumber = str(1)
         try:
             bestPost = galleryUrl + '&page=' + pageNumber + '&exception_mode=recommend'
-            return bestPost
+            getHtmlByte = urllib.request.urlopen(bestPost)
+            getHtmlString = str(getHtmlByte.read())
+            return getHtmlString
         except:
             print(traceback.format_exc())
             sys.exit()
